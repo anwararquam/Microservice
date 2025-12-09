4ecdaaa70d3df2fc6a5a63ba39fee0219c495b60
anwararquam <anwararquam5@gmail.com>
2025-12-10 01:23:12 +0530

    Microservice

---
+++ activityservice/src/main/java/com/example/activityservice/model/ActivityType.java
@@ -0,0 +1,5 @@
+package com.example.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,WALKING,CYCLING,SWIMMING,WEIGHT_TRAINING,YOGA,HIIT,CARDIO,STRETCHING,OTHER
+}
---
+++ activityservice/src/main/java/com/example/activityservice/service/ActivityService.java
@@ -0,0 +1,45 @@
+package com.example.activityservice.service;
+
+import com.example.activityservice.dto.ActivityRequest;
+import com.example.activityservice.dto.ActivityResponse;
+import com.example.activityservice.model.Activity;
+import com.example.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+    private final ActivityRepository activityRepository;
+    private final UserValidationService userValidationService;
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        boolean isValidUser=userValidationService.validateUser(request.getUserId());
+        if(!isValidUser){
+            throw new RuntimeException("Invalid User"+request.getUserId());
+        }
+        Activity activity=Activity.builder()
+                .userId(request.getUserId())
+                .caloriesBurned(request.getCaloriesBurned())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+        Activity savedActivity=activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity savedActivity) {
+        ActivityResponse response=new ActivityResponse();
+        response.setId(savedActivity.getId());
+        response.setUserId(savedActivity.getUserId());
+        response.setCaloriesBurned(savedActivity.getCaloriesBurned());
+        response.setType(savedActivity.getType());
+        response.setDuration(savedActivity.getDuration());
+        response.setStartTime(savedActivity.getStartTime());
+        response.setAdditionalMetrics(savedActivity.getAdditionalMetrics());
+        response.setCreatedAt(savedActivity.getCreatedAt());
+        response.setUpdatedAt(savedActivity.getUpdatedAt());
+        return response;
+    }
+}
